3aff45b36e313198d26e60f13957a1ba2645f776
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2021-04-22 22:20:01 +0200

    Added kubernetes.

--- home/modules/cloud/default.nix
+++ home/modules/cloud/default.nix
@@ -5,7 +5,14 @@
     path = "${config.home.homeDirectory}/Cloud";
   };
 
-  home.packages = with pkgs; [
-    aws
-  ];
+  home.packages = with pkgs; [ awscli kubectl krew ];
+
+  home.sessionVariables = {
+    AWS_SHARED_CREDENTIALS_FILE = "$XDG_CONFIG_HOME/aws/credentials";
+    AWS_CONFIG_FILE = "$XDG_CONFIG_HOME/aws/config";
+    PATH = "$PATH:$HOME/.krew/bin";
+  };
+
+  # Add an alias for kubectl
+  programs.zsh.shellAliases = { k = "kubectl"; };
 }
