bc944a2009b1c889bedecd345cd74cb81dafd090
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2021-10-05 13:13:16 +0200

    MOAR zsh customization.

--- home/modules/emacs/default.nix
+++ home/modules/emacs/default.nix
@@ -95,7 +95,7 @@
   # I cannot live without you, my one true love...
   programs.emacs = {
     enable = true;
-    package = pkgs.emacsGcc;
+    package = pkgs.emacsPgtkGcc;
     # For vterm.
     extraPackages = epkgs: [
       epkgs.vterm
