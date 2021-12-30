11e98bc7b98d8afb4de796a3d0c90ed40cfdf0a8
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2021-12-30 11:23:54 -0800

    fix(awesomewm): dir-local vars fixup.

--- home/modules/wm/conf/.dir-locals.el
+++ home/modules/wm/conf/.dir-locals.el
@@ -1,5 +1,5 @@
 ;;; Directory Local Variables
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((lua-mode . ((lsp-lua-workspace-library . ((/home/dlevym/.local/share/awesome/lib . t)))
+((lua-mode . ((lsp-lua-workspace-library . (("/home/dlevym/.local/share/awesome/lib" . t)))
               (lsp-lua-diagnostics-globals . ["awesome" "client" "root" "screen"]))))
