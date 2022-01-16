0c2fa1d5b070d1fbfec66e027d0a5576202d92cd
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2022-01-16 09:10:02 -0800

    chore: Refactoring `specialArgs`.

--- flake.nix
+++ flake.nix
@@ -7,7 +7,15 @@
   };
   outputs =
     { nixpkgs, nixpkgs-unstable, home-manager, emacs-overlay, ... }@inputs:
-    let system = "x86_64-linux";
+    let
+      system = "x86_64-linux";
+      specialArgs = {
+        emacs-overlay = emacs-overlay.overlay;
+        unstable = (import nixpkgs-unstable {
+          inherit system;
+          config.allowUnfree = true;
+        });
+      };
     in {
       nixosConfigurations = {
         dellXps15 = nixpkgs.lib.nixosSystem {
@@ -17,25 +25,13 @@
             ./common
             ./hosts/dellXps15
           ];
-          specialArgs = {
-            emacs-overlay = emacs-overlay.overlay;
-            unstable = (import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            });
-          };
+          inherit specialArgs;
         };
         nyx15v2 = nixpkgs.lib.nixosSystem {
           inherit system;
           modules =
             [ home-manager.nixosModules.home-manager ./common ./hosts/nyx15v2 ];
-          specialArgs = {
-            emacs-overlay = emacs-overlay.overlay;
-            unstable = (import nixpkgs-unstable {
-              inherit system;
-              config.allowUnfree = true;
-            });
-          };
+          inherit specialArgs;
         };
       };
     };
