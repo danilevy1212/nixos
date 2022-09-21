2fa62ed5f18fe8a6b7a70a4e5c505696606ca101
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2022-09-21 08:28:42 -0400

    feat(mpv): resume where left off

--- home/modules/apps/default.nix
+++ home/modules/apps/default.nix
@@ -1,19 +1,19 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }: {
   # Video Player
   programs.mpv = {
     enable = true;
     config = {
       sub-auto = "all";
+      save-position-on-quit = true;
     };
   };
 
   # default browser
-  home.sessionVariables = {BROWSER = "brave";};
+  home.sessionVariables = { BROWSER = "brave"; };
 
   imports = [
     ./linux-apps.nix
