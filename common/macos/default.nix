5ee027cb071dc2f2e0a1146465b1e3e9b709758c
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2022-09-21 18:04:21 -0400

    feat(macos): Show hidden files by default

--- common/macos/default.nix
+++ common/macos/default.nix
@@ -1,8 +1,7 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
+{ config
+, lib
+, pkgs
+, ...
 }: {
   home-manager.users.dlevy = import ./../../home/home.nix;
   users.users.dlevy.home = "/Users/dlevy";
@@ -34,7 +33,7 @@
       dejavu_fonts
       symbola
       noto-fonts
-      (import ./../../home/modules/emacs/quivera.nix {inherit pkgs;})
+      (import ./../../home/modules/emacs/quivera.nix { inherit pkgs; })
       # Terminal
       victor-mono
     ];
@@ -105,6 +104,16 @@
     keyboard = {
       enableKeyMapping = true;
       remapCapsLockToControl = true;
+    };
+    defaults = {
+      # Show hidden files in finder
+      finder = {
+        AppleShowAllFiles = true;
+      };
+      # Show hidden files, always
+      NSGlobalDomain = {
+        AppleShowAllFiles = true;
+      };
     };
   };
 
