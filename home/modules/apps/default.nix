d4c216ad98a31da0ffaaaa5de014a529070bccd2
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-05-12 12:41:01 -0400

    knit(home-manager.apps): Deprecate non-linux apps

--- home/modules/apps/default.nix
+++ home/modules/apps/default.nix
@@ -2,6 +2,7 @@
   config,
   lib,
   pkgs,
+  unstable,
   ...
 }: {
   # Video Player
@@ -16,7 +17,32 @@
   # default browser
   home.sessionVariables = {BROWSER = "brave";};
 
-  imports = [
-    ./linux-apps.nix
+  # Apps
+  home.packages = with pkgs; [
+    # Proprietary musicality
+    spotify
+
+    # Non-proprietary musicality
+    (import ./nuclear.nix pkgs)
+
+    # Social closeness
+    tdesktop
+    slack
+    teams
+
+    # Browser for the...
+    brave
+
+    # Keep my passwords safe
+    keepassxc
+
+    # I only use this to download Linux ISO images I super promise
+    qbittorrent
+
+    # Memorize
+    anki
+
+    # mpv + jellyfin, streaming heaven
+    unstable.jellyfin-mpv-shim
   ];
 }
