4b871b9982ad3d640a739eca320a1a1c15047d09
Daniel Levy Moreno <dalevy@autopay.com>
2023-09-09 16:38:27 -0400

    feature(apps): unityhub

--- home/modules/apps/default.nix
+++ home/modules/apps/default.nix
@@ -4,6 +4,8 @@
   pkgs,
   stable,
   unstable,
+  hostname,
+  HOSTS,
   ...
 }: {
   # Video Player
@@ -19,31 +21,37 @@
   home.sessionVariables = {BROWSER = "brave";};
 
   # Apps
-  home.packages = with pkgs; [
-    # Proprietary musicality
-    spotify
+  home.packages = with pkgs;
+    [
+      # Proprietary musicality
+      spotify
 
-    # Non-proprietary musicality
-    (import ./nuclear.nix pkgs)
+      # Non-proprietary musicality
+      (import ./nuclear.nix pkgs)
 
-    # Social closeness
-    tdesktop
-    slack
-    teams
+      # Social closeness
+      tdesktop
+      slack
+      teams
 
-    # Browser for the...
-    brave
+      # Browser for the...
+      brave
 
-    # Keep my passwords safe
-    keepassxc
+      # Keep my passwords safe
+      keepassxc
 
-    # I only use this to download Linux ISO images I super promise
-    qbittorrent
+      # I only use this to download Linux ISO images I super promise
+      qbittorrent
 
-    # Memorize
-    anki
+      # Memorize
+      anki
 
-    # mpv + jellyfin, streaming heaven
-    stable.jellyfin-mpv-shim
-  ];
+      # mpv + jellyfin, streaming heaven
+      stable.jellyfin-mpv-shim
+    ]
+    ++ (
+      if hostname == HOSTS.nyx15v2
+      then [unityhub]
+      else []
+    );
 }
