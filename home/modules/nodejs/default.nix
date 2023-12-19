66452b0058a80a285ab2669e20b58da14ac7bfc4
Daniel Levy Moreno <daniellevymoreno@gmail.com>
2023-12-19 17:04:50 -0500

    feat(fnm): deprecated nvm and added fnm

--- home/modules/nodejs/default.nix
+++ home/modules/nodejs/default.nix
@@ -1,35 +1,29 @@
-{
-  config,
-  lib,
-  pkgs,
-  environment,
-  ...
-}: {
-  home.packages = with pkgs; [
-    nodejs
-    (import ./corepack-wrapper.nix {
-      inherit pkgs;
-      inherit nodejs;
-    })
-  ];
-
+{pkgs, ...}: {
   # Move NPM Configuration from $HOME.
   home = {
     sessionVariables = {
       NPM_CONFIG_USERCONFIG = "$XDG_CONFIG_HOME/npm/npmrc";
       NPM_CONFIG_CACHE = "$XDG_CACHE_HOME/npm";
       NPM_CONFIG_TMP = "$XDG_RUNTIME_DIR/npm";
-      # NOTE: Not compatible with nvm
-      # NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
+      NPM_CONFIG_PREFIX = "$XDG_CACHE_HOME/npm";
       NODE_REPL_HISTORY = "$XDG_CACHE_HOME/node/repl_history";
 
       # YARN
       COREPACK_HOME = "$XDG_CACHE_HOME/corepack";
-    };
-  };
 
-  # Global packages readily usable.
-  home.sessionPath = [
-    "$XDG_CACHE_HOME/npm/bin"
-  ];
+      # FNM, auto-enable corepack
+      FNM_COREPACK_ENABLED = "true";
+    };
+    # Global packages readily usable.
+    sessionPath = [
+      "$XDG_CACHE_HOME/npm/bin"
+    ];
+
+    # Node
+    packages = with pkgs; [
+      # Node version manager
+      fnm
+      nodejs
+    ];
+  };
 }
