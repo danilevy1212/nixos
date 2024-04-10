a5f19190711e3d5cc29f902548a3352132d2a87b
Daniel Levy <daniellevymoreno@gmail.com>
2024-04-10 16:28:01 -0400

    feat(bootse): Use Nvidia 535 driver so wayland doesn't stutter

--- hosts/bootse/default.nix
+++ hosts/bootse/default.nix
@@ -14,7 +14,6 @@
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
-  boot.extraModulePackages = [config.boot.kernelPackages.nvidia_x11];
   boot.kernelParams = ["module_blacklist=i915" "nvidia_drm.fbdev=1"];
 
   # Enable networking
@@ -50,6 +49,31 @@
     modesetting.enable = true;
     nvidiaSettings = true;
     powerManagement.enable = true;
+    package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+      version = "535.129.03";
+      sha256_64bit = "sha256-5tylYmomCMa7KgRs/LfBrzOLnpYafdkKwJu4oSb/AC4=";
+      sha256_aarch64 = "sha256-i6jZYUV6JBvN+Rt21v4vNstHPIu9sC+2ZQpiLOLoWzM=";
+      openSha256 = "sha256-/Hxod/LQ4CGZN1B1GRpgE/xgoYlkPpMh+n8L7tmxwjs=";
+      settingsSha256 = "sha256-QKN/gLGlT+/hAdYKlkIjZTgvubzQTt4/ki5Y+2Zj3pk=";
+      persistencedSha256 = "sha256-FRMqY5uAJzq3o+YdM2Mdjj8Df6/cuUUAnh52Ne4koME=";
+      patches = with pkgs; let
+        # https://forums.developer.nvidia.com/t/linux-6-7-3-545-29-06-550-40-07-error-modpost-gpl-incompatible-module-nvidia-ko-uses-gpl-only-symbol-rcu-read-lock/280908/19
+        rcu_patch = fetchpatch {
+          url = "https://github.com/gentoo/gentoo/raw/c64caf53/x11-drivers/nvidia-drivers/files/nvidia-drivers-470.223.02-gpl-pfn_valid.patch";
+          hash = "sha256-eZiQQp2S/asE7MfGvfe6dA/kdCvek9SYa/FFGp24dVg=";
+        };
+        # https://gist.github.com/joanbm/24f4d4f4ec69f0c37038a6cc9d132b43
+        linux_6_8_patch = fetchpatch {
+          url = "https://gist.github.com/joanbm/24f4d4f4ec69f0c37038a6cc9d132b43/raw/bacb9bf3617529d54cb9a57ae8dc9f29b41d4362/nvidia-470xx-fix-linux-6.8.patch";
+          hash = "sha256-SPLC2uGdjHSy4h9i3YFjQ6se6OCdWYW6tlC0CtqmP50=";
+          extraPrefix = "kernel/";
+          stripLen = 1;
+        };
+      in [
+        rcu_patch
+        linux_6_8_patch
+      ];
+    };
   };
   hardware.opengl = {
     enable = true;
