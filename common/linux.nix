b72b078abe55a09590034907f918a52e359fb675
Daniel Levy <daniellevymoreno@gmail.com>
2026-09-13 06:48:08 -0400

    chore: bump flakes

--- common/linux.nix
+++ common/linux.nix
@@ -28,7 +28,7 @@ in {
     supportedFilesystems = ["ntfs"];
 
     # Keep things stable, mirror FEDORA
-    kernelPackages = lib.mkDefault pkgs.linuxPackages_7_1;
+    kernelPackages = lib.mkDefault pkgs.linuxPackages_7_2;
 
     # Clean /tmp on boot
     tmp.cleanOnBoot = true;
